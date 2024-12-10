13ff90613c562427d29b668aedb2485e2745b06f
pencc <pencc@example.com>
2024-12-10 08:50:58 +0800

    remove stupid compile/run git log

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -18,10 +18,6 @@ include $(NEMU_HOME)/scripts/build.mk
 
 include $(NEMU_HOME)/tools/difftest.mk
 
-compile_git:
-	$(call git_commit, "compile NEMU")
-$(BINARY):: compile_git
-
 # Some convenient rules
 
 override ARGS ?= --log=$(BUILD_DIR)/nemu-log.txt
@@ -34,11 +30,9 @@ NEMU_EXEC := $(BINARY) $(ARGS) $(IMG)
 run-env: $(BINARY) $(DIFF_REF_SO)
 
 run: run-env
-	$(call git_commit, "run NEMU")
 	$(NEMU_EXEC)
 
 gdb: run-env
-	$(call git_commit, "gdb NEMU")
 	gdb -s $(BINARY) --args $(NEMU_EXEC)
 
 clean-tools = $(dir $(shell find ./tools -maxdepth 2 -mindepth 2 -name "Makefile"))
