677c240e38aa297456b5c6fdce90da3a57d642da
pencc <pencc@example.com>
2025-11-19 17:33:44 +0800

    pass am-tests/rtc

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -2,10 +2,13 @@
 #include <nemu.h>
 
 void __am_timer_init() {
+  inl(RTC_ADDR);
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = 0;
+  uptime->us = inl(RTC_ADDR + 4);
+  uptime->us <<= 32;
+  uptime->us |= inl(RTC_ADDR);
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
