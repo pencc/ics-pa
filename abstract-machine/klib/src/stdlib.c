a6816a88b7d4a7eb043fd30329dec3c45c786513
pencc <pencc@example.com>
2025-12-04 15:36:41 +0800

    impl malloc/free

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -2,6 +2,9 @@
 #include <klib.h>
 #include <klib-macros.h>
 
+static char *hbrk;
+static bool _malloc_init = false;
+
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 static unsigned long int next = 1;
 
@@ -34,9 +37,21 @@ void *malloc(size_t size) {
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  panic("Not implemented");
+  if (!_malloc_init) {
+    hbrk = (void *)ROUNDUP(heap.start, 8);
+    _malloc_init = true;
+  }
+
+  size  = (size_t)ROUNDUP(size, 8);
+  char *old = hbrk;
+  hbrk += size;
+  assert((uintptr_t)heap.start <= (uintptr_t)hbrk && (uintptr_t)hbrk < (uintptr_t)heap.end);
+  for (uint64_t *p = (uint64_t *)old; p != (uint64_t *)hbrk; p ++) {
+    *p = 0;
+  }
+  return old;
 #endif
-  return NULL;
+    return NULL;
 }
 
 void free(void *ptr) {
