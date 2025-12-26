f051772714b3b0ec27031532f886ef4a4e503c0f
pencc <pencc@example.com>
2025-12-26 17:15:06 +0800

    add keyboard event device(/dev/events) in nanos-lite, support input event in libNDL, pass navy-apps/tests/event-test.

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -22,7 +22,14 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
 }
 
 size_t events_read(void *buf, size_t offset, size_t len) {
-  return 0;
+  AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
+
+  if(AM_KEY_NONE == ev.keycode)
+    return 0;
+
+  return snprintf((char *)buf, len, "%s %s\n",
+    ev.keydown ? "kd" : "ku",
+    keyname[ev.keycode]);;
 }
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
